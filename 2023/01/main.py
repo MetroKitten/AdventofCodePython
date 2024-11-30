e213aa03e80ebff4a502683dc4c1979803d93c54
MetroKitten <113661103+MetroKitten@users.noreply.github.com>
2024-11-30 14:43:44 +1100

    01 Part 2

--- 2023/01/main.py
+++ 2023/01/main.py
@@ -13,6 +13,21 @@ def Calibration(calibration_list: list[str]) -> int:
     return total
 
 
+def CalibrationV2(calibration_list: list[str]) -> int:
+    total: int = 0
+    for string in calibration_list:
+        num_only: list[str] = []
+        for char in string:
+            if char.isdigit():
+                num_only.append(char)
+        left: str = num_only[0]
+        right: str = num_only[-1]
+        actual: str = left + right
+        final = int(actual)
+        total = total + final
+    return total
+
+
 def main():
     with open("input.txt", "r") as f:
         calibration_list = [line.strip() for line in f]
--- 2023/01/test_main.py
+++ 2023/01/test_main.py
@@ -1,6 +1,6 @@
 import unittest
 
-from main import Calibration
+from main import Calibration, CalibrationV2
 
 
 class TestCalibration(unittest.TestCase):
@@ -8,4 +8,18 @@ class TestCalibration(unittest.TestCase):
         input = ["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"]
         expected = 142
         output = Calibration(input)
+        self.assertEqual(output, expected)
+
+    def test_CalibrationV2(self):
+        input = [
+            "two1nine",
+            "eightwothree",
+            "abcone2threexyz",
+            "xtwone3four",
+            "4nineeightseven2",
+            "zoneight234",
+            "7pqrstsixteen",
+        ]
+        expected = 281
+        output = CalibrationV2(input)
         self.assertEqual(output, expected)
