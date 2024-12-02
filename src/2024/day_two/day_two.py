cabb57316a27707c1b26a96c162cfe7ba46fab36
MetroKitten <113661103+MetroKitten@users.noreply.github.com>
2024-12-02 21:16:34 +1100

    Day 2

--- src/2024/day_two/day_two.py
+++ src/2024/day_two/day_two.py
@@ -1,4 +1,4 @@
-def safety_report(report_line: list[int]) -> bool:
+def is_safe_report(report_line: list[int]) -> bool:
     is_ascending = all(a < b for a, b in zip(report_line, report_line[1:]))
     is_descending = all(a > b for a, b in zip(report_line, report_line[1:]))
     right_diff = all(abs(a - b) <= 3 for a, b in zip(report_line, report_line[1:]))
@@ -10,12 +10,12 @@ def main():
         safe_reports: int = 0
         for line in f:
             part = [int(x) for x in line.split()]
-            if safety_report(part):
+            if is_safe_report(part):
                 safe_reports += 1
             else:
                 for i in range(len(part)):
                     chopped_list = part[:i] + part[i + 1 :]
-                    if safety_report(chopped_list):
+                    if is_safe_report(chopped_list):
                         safe_reports += 1
                         break
         print(f"Total Safe Reports {safe_reports}")
--- src/2024/day_two/test_day_two.py
+++ src/2024/day_two/test_day_two.py
@@ -1,43 +1,43 @@
 import unittest
 
-from day_two import safety_report
+from day_two import is_safe_report
 
 
 class SafetyReportTest(unittest.TestCase):
-    def test_safety_report1(self):
+    def test_is_safe_report1(self):
         reports = [7, 6, 4, 2, 1]
         expected = True
-        output = safety_report(reports)
+        output = is_safe_report(reports)
         self.assertEqual(output, expected)
 
-    def test_safety_report2(self):
+    def test_is_safe_report2(self):
         reports = [1, 2, 7, 8, 9]
         expected = False
-        output = safety_report(reports)
+        output = is_safe_report(reports)
         self.assertEqual(output, expected)
 
-    def test_safety_report3(self):
+    def test_is_safe_report3(self):
         reports = [9, 7, 6, 2, 1]
         expected = False
-        output = safety_report(reports)
+        output = is_safe_report(reports)
         self.assertEqual(output, expected)
 
-    def test_safety_report4(self):
+    def test_is_safe_report4(self):
         reports = [1, 3, 2, 4, 5]
         expected = False
-        output = safety_report(reports)
+        output = is_safe_report(reports)
         self.assertEqual(output, expected)
 
-    def test_safety_report5(self):
+    def test_is_safe_report5(self):
         reports = [8, 6, 4, 4, 1]
         expected = False
-        output = safety_report(reports)
+        output = is_safe_report(reports)
         self.assertEqual(output, expected)
 
-    def test_safety_report6(self):
+    def test_is_safe_report6(self):
         reports = [1, 3, 6, 7, 9]
         expected = True
-        output = safety_report(reports)
+        output = is_safe_report(reports)
         self.assertEqual(output, expected)
 
 
