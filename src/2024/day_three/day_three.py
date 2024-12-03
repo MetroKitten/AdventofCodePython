feb65643c24973ce0c7f0b18f8031543f09ba24b
MetroKitten <113661103+MetroKitten@users.noreply.github.com>
2024-12-03 22:53:39 +1100

    Day 3 - Part 2

--- src/2024/day_three/day_three.py
+++ src/2024/day_three/day_three.py
@@ -1,23 +1,39 @@
-import re
+import regex
 
 
-def find_muls(text: str):
+def find_muls(text: str) -> int:
     uncorrupted = 0
-    matches: list[tuple[str, str]] = re.findall(r"mul\((\d+),(\d+)\)", text)
+    matches: list[tuple[str, str]] = regex.findall(r"mul\((\d+),(\d+)\)", text)
     for match in matches:
         left, right = map(int, match)
         uncorrupted += left * right
     return uncorrupted
 
 
+def find_muls_v2(text: str) -> int:
+    matches: list[str] = regex.split(r"(?<=mul\(\d+,\d+\))", text)
+    disabled_strings: list[str] = []
+    enabled_strings: list[str] = []
+    enabled: bool = True
+    uncorrupted = 0
+    for line in matches:
+        if "don't()" in line:
+            enabled = False
+        elif "do()" in line:
+            enabled = True
+        if enabled:
+            enabled_strings.append(line)
+        else:
+            disabled_strings.append(line)
+    for string in enabled_strings:
+        uncorrupted += find_muls(string)
+    return uncorrupted
+
+
 def main():
     with open("./input.txt", "r") as f:
-        total = 0
-        for line in f:
-            matches = find_muls(line)
-            total += matches
-
-    print(f"Uncorrupted Total: {total}")
+        file = f.read()
+    print(f"Uncorrupted Total: {find_muls_v2(file)}")
 
 
 if __name__ == "__main__":
