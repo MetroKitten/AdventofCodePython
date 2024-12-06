090039fb8dd605be2e3eb06b917d8256e5fe8325
MetroKitten <113661103+MetroKitten@users.noreply.github.com>
2024-12-06 22:47:08 +1100

    Day 5

--- src/2024/day_five/day_five.py
+++ src/2024/day_five/day_five.py
@@ -8,6 +8,26 @@ def check_meets_rules(pages: list[int], rules: list[tuple[int, int]]) -> bool:
     return True
 
 
+def fix_failing_pages(pages: list[int], rules: list[tuple[int, int]]):
+    failed_page = pages.copy()
+    if check_meets_rules(failed_page, rules):
+        return failed_page
+
+    for high, low in rules:
+        if high in failed_page and low in failed_page:
+            high_index = failed_page.index(high)
+            low_index = failed_page.index(low)
+            if high_index >= low_index:
+                failed_page[high_index], failed_page[low_index] = (
+                    failed_page[low_index],
+                    failed_page[high_index],
+                )
+                result = fix_failing_pages(failed_page, rules)
+                if result:
+                    return result
+    return False
+
+
 def calc_middle_totals(correct_pages: list[int]) -> int:
     middle_total = 0
     if len(correct_pages) % 2 == 0:
@@ -21,6 +41,8 @@ def calc_middle_totals(correct_pages: list[int]) -> int:
 def main() -> None:
     with open("./input.txt", "r") as f:
         correct_pages_total: int = 0
+        fixed_pages_total: int = 0
+        failed_pages: list[list[int]] = []
         rules: list[tuple[int, int]] = []
         pages: list[list[int]] = []
         file = f.read()
@@ -37,8 +59,15 @@ def main() -> None:
         for page in pages:
             if check_meets_rules(page, rules):
                 correct_pages_total += calc_middle_totals(page)
+            else:
+                failed_pages.append(page)
+
+        for page in failed_pages:
+            corrected_page: list[int] = fix_failing_pages(page, rules)
+            fixed_pages_total += calc_middle_totals(corrected_page)
 
         print(f"Correct Page Total: {correct_pages_total}")
+        print(f"Fixed Page Total: {fixed_pages_total}")
 
 
 if __name__ == "__main__":
